c81adc17946741260df6e796fae75455714757ff
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-14 20:30:12 +0300

    Day 5, task 6

--- 5/CollectionWrapper.cs
+++ 5/CollectionWrapper.cs
@@ -16,5 +16,49 @@ namespace _5
             }
             return firstlist;
         }
+
+        public static bool operator ==(CollectionWrapper first, CollectionWrapper second)
+        {
+            if (first.list.Count != second.list.Count)
+            {
+                return false;
+            }
+            
+            for(int i = 0; i < first.list.Count; i++)
+            {
+                if (!first.list[i].Equals(second.list[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool operator !=(CollectionWrapper first, CollectionWrapper second)
+        {
+            if (first.list.Count != second.list.Count)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < first.list.Count; i++)
+            {
+                if (!first.list[i].Equals(second.list[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator <(CollectionWrapper first, CollectionWrapper second)
+        {
+            return first.list.Count < second.list.Count;
+        }
+
+        public static bool operator >(CollectionWrapper first, CollectionWrapper second)
+        {
+            return first.list.Count > second.list.Count;
+        }
     }
 }
