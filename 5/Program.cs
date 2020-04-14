0c803c11e7ca5ac6deac88b34a6701ce81cfc58d
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-14 20:07:35 +0300

    Day 5, task 4

--- 5/Program.cs
+++ 5/Program.cs
@@ -13,14 +13,18 @@ namespace _5
         Item1 = 0x0001,
         Item2 = 0x0002
     }
+
+    delegate void Actions();
     class Program
     {
         static E e;
         static BitFlags b = BitFlags.Item1;
+        static Actions actions;
         static void Main(string[] args)
         {
             MethodEnum(ref e);
             MethodBitFlag(ref b);
+            MethodDelegate((1==1.0), actions);
         }
 
         static void MethodEnum(ref E e) // Task 1 method
@@ -36,6 +40,21 @@ namespace _5
         static bool MethodBitFlag(BitFlags sequence, BitFlags flag) // Task 3
         {
             return (sequence & flag) == flag;
+        }
+        
+        // Task 4
+        static void MethodDelegate(bool condition, Actions actions)
+        {
+            if (condition)
+            {
+                actions += SomeAction;
+            }
+            actions();
+        }
+
+        static void SomeAction()
+        {
+            Console.WriteLine("Action");
         }
     }
 }
