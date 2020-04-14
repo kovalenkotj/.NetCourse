f3efca6ffc3c84220366205854142234345aa67a
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-14 20:19:25 +0300

    Day 5, task 5

---
+++ 5/CollectionWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5
+{
+    class CollectionWrapper
+    {
+        List<int> list = new List<int>();
+
+        public static CollectionWrapper operator +(CollectionWrapper firstlist, CollectionWrapper secondlist)
+        {
+            foreach(int i in secondlist.list)
+            {
+                firstlist.list.Add(i);
+            }
+            return firstlist;
+        }
+    }
+}
