b068d2d413124632e0f869a6f516472ac0517c97
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-14 10:45:48 +0300

    Day 4, task 1

---
+++ 4/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4
+{
+    class A
+    {
+        // Task 1
+        public int Prop { private get; set; }
+        public int Prop1 { get; private set; }
+        //public int Prop2 { private get; private set; } //mistake, because we can't apply access modifiers to both accessors
+        public int Prop3 { get; protected set; }
+        //protected int Prop4 { get; public set; } //mistake, because the accessor can't be more visible than the property in general
+        protected int Prop5 { get; private set; }
+        //protected int Prop6 { get; internal set; } 
+        //internal int Prop7 { get; protected set; } //tha same mistakes as Prop4
+        protected internal int Prop8 { get; protected set; }
+        protected internal int Prop9 { get; internal set; }
+        //public int Prop10 { public get; set; } //mistake, because accessor must be more restrictive than the property in general
+
+    }
+}
