ba84ca167f02cb3212ea71261c433c1539d20aa4
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-14 19:50:31 +0300

    Day 5, tasks 1,2

---
+++ 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5
+{
+    enum E
+    {
+        Item1,
+        Item2
+    }
+    [Flags]
+    enum BitFlags
+    {
+        Item1 = 0x0001,
+        Item2 = 0x0002
+    }
+    class Program
+    {
+        static E e;
+        static BitFlags b = BitFlags.Item1;
+        static void Main(string[] args)
+        {
+            MethodEnum(ref e);
+            MethodBitFlag(ref b);
+        }
+
+        static void MethodEnum(ref E e) // Task 1 method
+        {
+            Console.WriteLine(e);
+        }
+
+        static void MethodBitFlag(ref BitFlags b)
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
