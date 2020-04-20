47c6e269d625f8e6ffabca5e7b4ae27b8dd58a16
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-20 18:01:10 +0300

    4th task - "Fill a collection with 10000 items. Enumerate this collection with for and foreach statements. Make a speed test for both of operators (you may use StopWatch class to calculate a time). Explain the difference of results"
    5th task - "In foreach body try to modify collection (add/remove element). Do the same in body of for statement. In which cycle it is possible to do this? Explain why"
    Question: what collection to use to see the difference between 'for' and 'foreach'?

---
+++ 2ndWeek/CollectionClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace _2ndWeek
+{
+    class CollectionClass
+    {
+        public static List<int> collection = new List<int>();
+
+        static CollectionClass()
+        {
+            for(int i = 0; i < 10000; i++)
+            {
+                collection.Add(i);
+            }
+        }
+
+        public static void Enumeration(out long result1, out long result2)
+        {
+            Stopwatch s1, s2;
+
+            s1 = Stopwatch.StartNew();
+            for(int i = 0; i < collection.Count; i++) { }
+            s1.Stop();
+            result1 = s1.ElapsedTicks;
+
+            s2 = Stopwatch.StartNew();
+            foreach(int i in collection) { }
+            s2.Stop();
+            result2 = s2.ElapsedTicks;
+        }
+
+        public static void Modifying(out List<int> collection1, out List<int> collection2)
+        {
+            collection1 = new List<int>(collection);
+            collection2 = new List<int>(collection);
+            for (int i = 0; i < collection.Count; i++)
+            {
+                collection1.RemoveAt(i);
+                break;
+            }
+
+            foreach(int i in collection)
+            {
+                collection2.Remove(i);
+                break;
+            }
+        }
+    }
+}
--- 2ndWeek/Program.cs
+++ 2ndWeek/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _2ndWeek
 {
@@ -6,7 +7,16 @@ namespace _2ndWeek
     {
         static void Main(string[] args)
         {
+            // Task 4
+            long r1, r2;
+            CollectionClass.Enumeration(out r1, out r2);
+            Console.WriteLine(r1 + " " + r2);
+            /* 'foreach' is slower than 'for', because creates and uses IEnumerator object during the execution*/
 
+            // Task 5
+            List<int> collection1, collection2;
+            CollectionClass.Modifying(out collection1, out collection2);
+            /* 'List<>' allows to use both 'for' and 'foreach' for modifying */
         }
     }
 }
