c7864986973bcc71d0ae9163562d00d878fd4069
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-20 15:05:06 +0300

    2nd task - "Create a custom class that implements the necessary interfaces to allow an array of the class to be sorted"

---
+++ 2ndWeek/ClassToSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _2ndWeek
+{
+    class ClassToSort : IComparable<ClassToSort>, IComparer<ClassToSort>
+    {
+        int someValue;
+        public int Compare(ClassToSort x, ClassToSort y)
+        {
+            if (x.someValue < y.someValue)
+            {
+                return -1;
+            }
+            else if (x.someValue > y.someValue)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public int CompareTo(ClassToSort other)
+        {
+            if (someValue < other.someValue)
+            {
+                return -1;
+            }
+            else if (someValue > other.someValue)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
