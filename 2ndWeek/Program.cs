d09d0ea07f8a2ff808f9a47624009af743987c45
Tetiana Kovalenko <tjk10410@gmail.com>
2020-04-27 18:03:38 +0300

    just for convenience

--- 2ndWeek/Program.cs
+++ 2ndWeek/Program.cs
@@ -10,20 +10,24 @@ namespace _2ndWeek
         static void Main(string[] args)
         {
             // Task 4
+            Console.WriteLine("\tTask 4");
             long r1, r2;
             CollectionClass.Enumeration(out r1, out r2);
             Console.WriteLine(r1 + " " + r2);
             /* 'foreach' is slower than 'for', because creates and uses IEnumerator object during the execution*/
 
             // Task 5
+            Console.WriteLine("\n\tTask 5");
             List<int> collection1, collection2;
             CollectionClass.Modifying(out collection1, out collection2);
             /* 'List<>' allows to use both 'for' and 'foreach' for modifying */
 
             // Task 7
+            Console.WriteLine("\n\tTask 7");
             Console.WriteLine(GenericsClass<int>.FillTime(5) + " " + ObjectsClass.FillTime(5));
 
             // Task 6
+            Console.WriteLine("\n\tTask 6");
             LinkedListClass<int> intList = new LinkedListClass<int>();
             intList.Add(5);
             intList.Add(8);
@@ -35,16 +39,19 @@ namespace _2ndWeek
             strList.Remove("str2");
 
             // Task 3
+            Console.WriteLine("\n\tTask 3");
             ClassForConvertion c = new ClassForConvertion(-5679999);
             Console.Write(c.ToString());
             Console.WriteLine();
 
             // Task 11
+            Console.WriteLine("\n\tTask 11");
             Console.Write(c.CustomAttributedMethods());
             Console.WriteLine();
 
 
             // Task 10
+            Console.WriteLine("\n\tTask 10");
             Type type = Type.GetType("_2ndWeek.ClassWithPrivateConstructor");
             // It works
             object obj = Activator.CreateInstance(type, true);
@@ -61,6 +68,7 @@ namespace _2ndWeek
             //}
 
             // Task 9
+            Console.WriteLine("\n\tTask 9");
             Type type1 = Type.GetType("_2ndWeek.StaticClass");
             MethodInfo[] methods = type1.GetMethods(BindingFlags.NonPublic|BindingFlags.Static);
             foreach(MethodInfo m in methods)
@@ -77,6 +85,7 @@ namespace _2ndWeek
             Console.WriteLine();
 
             // Task 13
+            Console.WriteLine("\n\tTask 13");
             object o = UniversalConstructorClass.CreateInstance("_2ndWeek.StaticClass");
             object o1 = UniversalConstructorClass.CreateInstance("_2ndWeek.ClassForConvertion");
             object o2 = UniversalConstructorClass.CreateInstance("System.Reflection.BindingFlags");
